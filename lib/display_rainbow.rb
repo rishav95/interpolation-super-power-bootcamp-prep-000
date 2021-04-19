0431e60061718d3e0cdd5a5d6d753933e771eb93
Rishav Maskey <rishav.maskey@hotmail.com>
2021-04-19 05:09:02 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,5 +1,9 @@
 # Write your #display_rainbow method here
 
 def display_rainbow arg
-  puts arg.map {|str| "#{str[0,1].upcase}: #{str}"}.join ', '
+  begin
+    puts arg.map {|str| "#{str[0,1].upcase}: #{str}"}.join ', '
+  rescue
+    puts "Error"
+  end
 end
